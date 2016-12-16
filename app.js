cc999481897fab5c03b887356cde596899cd6b9c
Joe Fearnley <joe.fearnley@gmail.com>
2016-12-16 14:32:09 +0000

    Adding some debugging stuff

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var Mailgun = require('mailgun-js');
-var dateFormat = require('dateformat');
+const request = require('request');
+const cheerio = require('cheerio');
+const Mailgun = require('mailgun-js');
+const dateFormat = require('dateformat');
+const url = 'https://www.packtpub.com/packt/offers/free-learning';
 
 require('dotenv').config();
 
-var url = 'https://www.packtpub.com/packt/offers/free-learning';
-
 request(url, processResponse);
-
 
 function processResponse(error, response, html) {
     if (!error) {
         var $ = cheerio.load(html);
         var title = $('.dotd-title > h2').html();
+
+        // var data = {
+        //     title: $('.dotd-title > h2').html(),
+        // };
+        
+        console.log(title);
     }    
 }
 
-function sendMail(title) {
+function sendMail(dat) {
     var mailgun = new Mailgun({
         apiKey: process.env.MAILGUN_API_KEY,
         domain: process.env.MAILGUN_DOMAIN
@@ -27,8 +31,8 @@ function sendMail(title) {
     var date = dateFormat(now, "dddd, mmmm dS, yyyy");
     
     var body = '<h3> PackPub Free Book of the Day</h3>' + 
-        '<p>The title of the free book today is <strong>' +
-        title + '</strong> </p> <p> Check it out here: ' + url;
+        '<p>The title of the free book today is <strong>' + title + '</strong></p>' + 
+        '<p> Check it out here: ' + url;
 
     var data = {
         from: process.env.MAILGUN_FROM,
