556bf23d359908c631bd4e4722e00e584563d168
joe.fearnley@gmail.com <joef@thestowcompany.com>
2020-11-17 20:30:12 -0500

    Adding updated query selector for cheerio

--- app.js
+++ app.js
@@ -8,46 +8,24 @@ const url = 'https://www.packtpub.com/free-learning';
 
 require('dotenv').config();
 
-phantom.create().then(function(ph) {
-    ph.createPage().then(function(page) {
-         page.open(url).then(function(status) {
-            page.property('content').then(function(content) {
-                const $ = cheerio.load(content);
-                const title = $('#free-learning-dropin .product > .product__info > .product__title')
-                    .html()
-                    .replace('Free eBook:  ','');
-
-                if (title === null || title.trim() === '') {
-                    console.log('No title found today, exiting.');
-                    return false;
-                }
-        
-                sendMail(formatMessageData(title));
-
-                page.close();
-                ph.exit();
-            });
-        });
-    });
-});
-
-const formatMessageData = (title) => {
+const formatMessageData = title => {
     const body = `
         <h3> PackPub Daily Free eBook</h3>
         <p>The title of the free book today is <strong>${title}</strong></p>
         <p> Check it out here: ${url}
     `;
 
-    const subject = `PackPub Free Book of the Day for ${dateFormat(new Date(), "dddd, mmmm dS, yyyy")}`;
+    const date = dateFormat(new Date(), "dddd, mmmm dS, yyyy");
+    const subject = `PackPub Free Book of the Day for ${date}`;
 
     return {
-        title: title,
-        body: body,
-        subject: subject
+        title,
+        body,
+        subject
     };
 }
 
-const sendMail = (args) => {
+const sendMail = args => {
     const mailgun = new Mailgun({
         apiKey: process.env.MAILGUN_API_KEY,
         domain: process.env.MAILGUN_DOMAIN
@@ -69,3 +47,25 @@ const sendMail = (args) => {
         console.log('Mail successfully sent!');
     });
 }
+
+(async function() {
+    const instance = await phantom.create();
+    const page = await instance.createPage();
+    const status = await page.open(url);
+    const content = await page.property('content');
+
+    const $ = cheerio.load(content);
+    const title = $('.product-info__content > .product-info__title')
+        .html()
+        .replace('Free eBook - ','');
+
+    if (title === null || title.trim() === '') {
+        console.log('No title found today, exiting.');
+        return false;
+    }
+
+     sendMail(formatMessageData(title));
+
+    await page.close();
+    await instance.exit();
+}());
